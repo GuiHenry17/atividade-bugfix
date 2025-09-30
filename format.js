b238972d5224c85581a9150a32d111bba79a7d2d
GuiHenry17 <gui201629@gmail.com>
2025-09-30 13:25:59 -0400

    Corrigir bug na formatação da data

--- format.js
+++ format.js
@@ -1,3 +1,7 @@
+// buggy-file.js
 function formatarData(data) {
-    return data.toISOString().splt('T')[0];
+    if (!(data instanceof Date) || isNaN(data)) {
+        throw new Error('Data inválida');
+    }
+    return data.toISOString().split('T')[0];
 }
